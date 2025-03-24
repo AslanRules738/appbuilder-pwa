26f99dfe66f9263db7630b0dcb14a67e8babbf10
AslanRules <39661498+AslanRules@users.noreply.github.com>
2025-03-24 13:56:23 -0400

    work on later

--- src/lib/scripts/tests/about.spec.ts
+++ src/lib/scripts/tests/about.spec.ts
@@ -20,31 +20,40 @@ test('sfdds', async ({ page }) => {
     await page.getByTestId('hamburger-icon').click();
     await page.getByRole('button', { name: "Settings" }).last().click();
 
-    await expect(await page.locator('div.mt').textContent()).toEqual("John");
-    //await expect(page.locator('[class="mt2"]')).toHaveText("The Good News According to");
-
-
-    await expect(page.getByText('Verse layout')).toBeVisible();
-    const selbox1 = page.getByRole('combobox').filter({ has: page.getByRole('option').filter({ hasText: 'Verses in paragraphs' }) });
+    /*await expect(page.getByText('User Interface')).toBeVisible();
+    const selbox1 = await page.getByRole('combobox').filter({ has: page.getByRole('option').filter({ hasText: 'Language' }) });
     await expect(selbox1).toBeVisible();
-    await expect(selbox1).toHaveValue("paragraphs");
-    await selbox1.selectOption("one-per-line");
-    await expect(selbox1).toHaveValue("one-per-line");
+    await expect(selbox1).toHaveValue("en");
+    await selbox1.selectOption("yao");
+    await expect(selbox1).toHaveValue("yao");*/
 
-    await expect(page.getByText('Images in Bible text')).toBeVisible();
-    const selbox2 = page.getByRole('combobox').filter({ has: page.getByRole('option').filter({ hasText: 'Show images' }) });
-    await expect(selbox2).toBeVisible();
-    await expect(selbox2).toHaveValue("normal");
-    await selbox2.selectOption("hidden");
-    await expect(selbox2).toHaveValue("hidden");
+    let prj = "DAB";
+    if (prj == "SAB") {
+        await expect(await page.locator('div.mt').textContent()).toEqual("John");
+        //await expect(page.locator('[class="mt2"]')).toHaveText("The Good News According to");
 
-    await expect(page.getByText('Videos in Bible text')).toBeVisible();
-    const selbox3 = page.getByRole('combobox').filter({ has: page.getByRole('option').filter({ hasText: 'Show normal size' }) });
-    await expect(selbox3).toBeVisible();
-    await expect(selbox3).toHaveValue("normal");
-    await selbox3.selectOption("hidden");
-    await expect(selbox3).toHaveValue("hidden");
+        await expect(page.getByText('Verse layout')).toBeVisible();
+        const selbox1 = page.getByRole('combobox').filter({ has: page.getByRole('option').filter({ hasText: 'Verses in paragraphs' }) });
+        await expect(selbox1).toBeVisible();
+        await expect(selbox1).toHaveValue("paragraphs");
+        await selbox1.selectOption("one-per-line");
+        await expect(selbox1).toHaveValue("one-per-line");
 
-    await page.getByTestId('back-button').click();
-    await expect(page.getByText('The Good News According To')).toBeVisible();
+        await expect(page.getByText('Images in Bible text')).toBeVisible();
+        const selbox2 = page.getByRole('combobox').filter({ has: page.getByRole('option').filter({ hasText: 'Show images' }) });
+        await expect(selbox2).toBeVisible();
+        await expect(selbox2).toHaveValue("normal");
+        await selbox2.selectOption("hidden");
+        await expect(selbox2).toHaveValue("hidden");
+
+        await expect(page.getByText('Videos in Bible text')).toBeVisible();
+        const selbox3 = page.getByRole('combobox').filter({ has: page.getByRole('option').filter({ hasText: 'Show normal size' }) });
+        await expect(selbox3).toBeVisible();
+        await expect(selbox3).toHaveValue("normal");
+        await selbox3.selectOption("hidden");
+        await expect(selbox3).toHaveValue("hidden");
+
+        await page.getByTestId('back-button').click();
+        await expect(page.getByText('The Good News According To')).toBeVisible();
+    }
 });
